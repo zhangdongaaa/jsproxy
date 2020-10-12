6251ba1dc61b58732d9eb383eec513b44dfbc5bd
zhangdong <15075821022@163.com>
2020-10-12 10:56:38 +0800

    zd

--- conf.js
+++ conf.js
@@ -23,7 +23,7 @@ jsproxy_config({
     'demo-sg': {
       label: '演示服务-新加坡节点',
       lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
+        '172.16.180.221.xip.io:8080': 1,
       },
     },
     'mysite': {
